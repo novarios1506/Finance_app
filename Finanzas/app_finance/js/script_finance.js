128ec678e1a35cac166bb3c8d16d2803fc9f5533
Francesco <novarios1506@gmail.com>
2023-04-30 00:26:52 -0500

    Finance

--- Finanzas/app_finance/js/script_finance.js
+++ Finanzas/app_finance/js/script_finance.js
@@ -53,6 +53,150 @@ logoutElement.addEventListener('click', () => {
   }))
 });
 
+// Obtener todos los enlaces <a> en la barra lateral
+const sidebarLinks = document.querySelectorAll('.sidebar a');
+
+// Obtener el elemento con la clase "middle"
+const middleElement = document.querySelector('.middle');
+const firstOperationElement = document.querySelector('.first_operation');
+const secondOperationElement = document.querySelector('.second_operation');
+const thirdOperationElement = document.querySelector('.third_operation');
+const quarterOperationElement = document.querySelector('.quarter_operation');
+const fifthOperationElement = document.querySelector('.fifth_operation');
+const sixthOperationElement = document.querySelector('.sixth_operation');
+const seventhOperationElement = document.querySelector('.seventh_operation');
+
+//Al iniciar la pagina los estilos de todos son bloqueados menos el middleElement
+middleElement.style.display = 'block';
+firstOperationElement.style.display = 'none';
+secondOperationElement.style.display = 'none';
+thirdOperationElement.style.display = 'none';
+quarterOperationElement.style.display = 'none';
+fifthOperationElement.style.display = 'none';
+sixthOperationElement.style.display = 'none';
+seventhOperationElement.style.display = 'none';
+
+// Agregar un controlador de eventos de clic a cada enlace <a> excepto el enlace "Logout"
+sidebarLinks.forEach(link => {
+  // Verificar si el enlace <a> actual tiene la clase "logout"
+  if (!link.classList.contains('logout')) {
+    link.addEventListener('click', event => {
+      // Verificar si el enlace <a> que se hizo clic ya tiene la clase "active"
+      if (!event.currentTarget.classList.contains('active')) {
+        // Eliminar la clase "active" del enlace <a> actualmente activo
+        document.querySelector('.sidebar a.active').classList.remove('active');
+        
+        // Agregar la clase "active" al enlace <a> que se hizo clic
+        event.currentTarget.classList.add('active');
+      }
+      
+      // Verificar si se hizo clic en el enlace "Exchange"
+      if (event.currentTarget.querySelector('h4').textContent === 'Exchange') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'block';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Dashboard"
+      if (event.currentTarget.querySelector('h4').textContent === 'Dashboard') {
+        // Mostrar el elemento con la clase "middle"
+        middleElement.style.display = 'block';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Wallet"
+      if (event.currentTarget.querySelector('h4').textContent === 'Wallet') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'block';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Transactions"
+      if (event.currentTarget.querySelector('h4').textContent === 'Transactions') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'block';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Analytics"
+      if (event.currentTarget.querySelector('h4').textContent === 'Analytics') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'block';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Messages"
+      if (event.currentTarget.querySelector('h4').textContent === 'Messages') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'block';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'none';
+      }
+
+      // Verificar si se hizo clic en el enlace "Help Center"
+      if (event.currentTarget.querySelector('h4').textContent === 'Help Center') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'block';
+        seventhOperationElement.style.display = 'none';
+      }
+      
+      // Verificar si se hizo clic en el enlace "Settings"
+      if (event.currentTarget.querySelector('h4').textContent === 'Settings') {
+        // Ocultar el elemento con la clase "middle"
+        middleElement.style.display = 'none';
+        firstOperationElement.style.display = 'none';
+        secondOperationElement.style.display = 'none';
+        thirdOperationElement.style.display = 'none';
+        quarterOperationElement.style.display = 'none';
+        fifthOperationElement.style.display = 'none';
+        sixthOperationElement.style.display = 'none';
+        seventhOperationElement.style.display = 'block';
+      }
+
+    });
+  }
+});
 
 // show or hide sidebar
 const menuBtn = document.querySelector('#menu-btn');
@@ -71,10 +215,21 @@ closeBtn.addEventListener('click', () => {
 
 // change theme
 const themeBtn = document.querySelector('.theme-btn');
+// Obtener el elemento <img> del logotipo
+const logoImg = document.querySelector('.logo');
 
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 
     themeBtn.querySelector('span:first-child').classList.toggle('active');
     themeBtn.querySelector('span:last-child').classList.toggle('active');
+
+    // Verificar si el cuerpo del documento tiene la clase "dark-theme"
+    if (document.body.classList.contains('dark-theme')) {
+      // Cambiar el atributo src del elemento <img> del logotipo a la imagen del tema oscuro
+      logoImg.src = './images/logo2.png';
+    } else {
+      // Cambiar el atributo src del elemento <img> del logotipo a la imagen del tema claro
+      logoImg.src = './images/logo.png';
+    }
 })
